6c0abb40c70af6a7a7ebaaa6040d4f595d9c5673
Ali Madooei <madooei@jhu.edu>
2026-03-07 21:46:37 -0500

    counter-05: Add event handlers (UI doesn't update yet)

--- counter-react/src/App.jsx
+++ counter-react/src/App.jsx
@@ -1,14 +1,29 @@
 import "./App.css";
 
 export default function App() {
-  const count = 0;
+  let count = 0;
+
+  function increment() {
+    count = count + 1;
+    console.log("count is now:", count);
+  }
+
+  function decrement() {
+    count = count - 1;
+    console.log("count is now:", count);
+  }
+
+  function reset() {
+    count = 0;
+    console.log("count is now:", count);
+  }
 
   return (
     <div className="counter">
       <h1>Count: {count}</h1>
-      <button>+</button>
-      <button>&minus;</button>
-      <button>Reset</button>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>−</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
